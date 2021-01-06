0e1dee4eb1728edafc9f6141ff280f6d51678d0f
allenorton <norton.allene@gmail.com>
2021-01-06 16:17:37 -0600

    name animations

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,13 +1,39 @@
 import { Animated } from "react-animated-css";
+import React, { Component } from 'react'
 
-function Intro() {
-    return (
-        <div className="Intro">
-            <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
-                <h1>ALLENE</h1>
-            </Animated>
-        </div>
-    );
+class Intro extends Component {
+    state = {
+        show: "none"
+    }
+
+    setShow = () => {
+        setTimeout(() => { this.setState({ show: "inline" }) }, 2000)
+    }
+    render() {
+        return (
+            <div className="Intro">
+                <div className="firstname">
+                    <Animated
+                        animationIn="slideInDown"
+                        // animationInDelay={2000}
+                        animationInDuration={1000}
+                        animationOut="fadeOut"
+                        isVisible={true}>
+                        <h1>ALLENE</h1>
+                    </Animated>
+                </div>
+                {this.setShow()}
+                <div className="lastname" style={{ display: this.state.show }}>
+                    <Animated
+                        animationIn="slideInDown"
+                        animationInDuration={1000}
+                        isVisible={true}>
+                        <p>NORTON</p>
+                    </Animated>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Intro;
