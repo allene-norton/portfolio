6d523501ada27c73aa3d485899cee152d34cd38f
allenorton <norton.allene@gmail.com>
2021-01-06 16:47:00 -0600

    added short desc and animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from './containers/Home'
 import './App.css';
+import '/Users/a_norton/Documents/job_search/portfolio/allene-portfolio/node_modules/react-typist/dist/Typist.css'
 
 function App() {
   return (
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,13 +1,19 @@
 import { Animated } from "react-animated-css";
 import React, { Component } from 'react'
+import Typist from 'react-typist';
+
 
 class Intro extends Component {
     state = {
-        show: "none"
+        show: "none",
+        showDesc: "none"
     }
 
     setShow = () => {
         setTimeout(() => { this.setState({ show: "inline" }) }, 2000)
+    }
+    setShowDesc = () => {
+        setTimeout(() => { this.setState({ showDesc: "inline" }) }, 3000)
     }
     render() {
         return (
@@ -30,7 +36,15 @@ class Intro extends Component {
                         isVisible={true}>
                         <p>NORTON</p>
                     </Animated>
+                    {this.setShowDesc()}
+                    <div className="intro-desc" style={{ display: this.state.showDesc }}>
+                        <Typist avgTypingDelay={35}>
+                            <Typist.Delay ms={3500} />
+                            Full stack developer focused on creating smooth applications with clean design.
+                    </Typist>
+                    </div>
                 </div>
+
             </div>
         );
     }
