13a3203be652a13d827fb732d9e2b27bdaccfcf4
allenorton <norton.allene@gmail.com>
2021-01-07 17:49:40 -0600

    added project gifs and logic

--- src/components/ShowProject.js
+++ src/components/ShowProject.js
@@ -1,9 +1,31 @@
 import { Animated } from "react-animated-css";
+import resitown from '../Images/resitowndemogif.gif'
+import viness from '../Images/vinessdemogif.gif'
+import playbase from '../Images/playbasedemogif.gif'
+import crecipe from '../Images/crecipedemogif.gif'
+import journalwave from '../Images/journalwavedemogif.gif'
+
+
 
 
 
 
 const ShowProject = (props) => {
+    function defineGif() {
+        if (props.project === 'resitown') {
+            return resitown
+        } else if (props.project === 'viness') {
+            return viness
+        } else if (props.project === 'playbase') {
+            return playbase
+        } else if (props.project === 'crecipe') {
+            return crecipe
+        } else if (props.project === 'journalwave') {
+            return journalwave
+        } else {
+            return ''
+        }
+    }
     return (
         <div className="show-project">
             <Animated
@@ -15,6 +37,7 @@ const ShowProject = (props) => {
                 <p>{props.project.toUpperCase()}</p>
             </Animated>
             <div className="project-desc">
+                <img className="gif" src={defineGif()} />
                 <p>
                     project description
             </p>
